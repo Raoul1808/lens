54926977040ec6d13e1155b17891607273c8c0b0
Raoul1808 <raoulthegeek@gmail.com>
2022-08-29 12:05:08 +0200

    Add SDL2 submodule

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
-#include <iostream>
+#include "SDL.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* window = SDL_CreateWindow("lens", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    bool running = true;
+    SDL_Event ev;
+    while (running)
+    {
+        while (SDL_PollEvent(&ev))
+        {
+            if (ev.type == SDL_QUIT)
+            {
+                running = false;
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
