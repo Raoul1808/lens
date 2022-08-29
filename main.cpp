30a7d0d70378bac4a252d8e585b513e32cf263f9
Raoul1808 <raoulthegeek@gmail.com>
2022-08-29 14:43:28 +0200

    Add OpenGL 4.6 (Glad)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
+#include "glad/glad.h"
 #include "SDL.h"
+#include <cstdio>
 
 int main()
 {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("lens", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    SDL_Window* window = SDL_CreateWindow("lens", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+    if (!gladLoadGLLoader(SDL_GL_GetProcAddress))
+    {
+        return -1;
+    }
+
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    printf("%u\n", buffer);
 
     bool running = true;
     SDL_Event ev;
@@ -19,12 +41,16 @@ int main()
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
+        glClearColor(0.390625f, 0.58203125f, 0.92578125f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
     }
 
-    SDL_DestroyRenderer(renderer);
+    glDeleteBuffers(1, &buffer);
+    glDeleteVertexArrays(1, &vao);
+
+    SDL_GL_DeleteContext(glContext);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
