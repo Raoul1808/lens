44dd3d09d1483aacd90012ed3bd0d4ec549e905f
Raoul1808 <raoulthegeek@gmail.com>
2022-08-29 21:09:28 +0200

    Boring Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "glad/glad.h"
 #include "SDL.h"
-#include <cstdio>
 
 int main()
 {
@@ -24,9 +23,58 @@ int main()
     glGenVertexArrays(1, &vao);
     glBindVertexArray(vao);
 
+
+    float vertices[] = {
+            -0.5f, -0.5f,
+            0.0f, 0.5f,
+            0.5f, -0.5f,
+    };
+
     unsigned int buffer;
     glGenBuffers(1, &buffer);
-    printf("%u\n", buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    const char* vertexShader = R"glsl(
+#version 460 core
+
+in vec2 position;
+
+void main()
+{
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+    )glsl";
+
+    const char* fragmentShader = R"glsl(
+#version 460 core
+
+out vec4 color;
+
+void main()
+{
+    color = vec4(1.0, 0.0, 0.0, 1.0);
+}
+    )glsl";
+
+    unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertexShader, nullptr);
+    glCompileShader(vs);
+    unsigned int fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragmentShader, nullptr);
+    glCompileShader(fs);
+
+    unsigned int program = glCreateProgram();
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    glUseProgram(program);
+
+
 
     bool running = true;
     SDL_Event ev;
@@ -44,9 +92,12 @@ int main()
         glClearColor(0.390625f, 0.58203125f, 0.92578125f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         SDL_GL_SwapWindow(window);
     }
 
+    glDeleteProgram(program);
     glDeleteBuffers(1, &buffer);
     glDeleteVertexArrays(1, &vao);
 
